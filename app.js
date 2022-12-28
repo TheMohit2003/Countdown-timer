e1e7693343f2c8b10d0551da804e324885f2dd95
mohit <mohitpardeshi729@gmail.com>
2022-12-28 21:15:34 +0530

    removed src folder and digitConvertor.js file

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-import digitConvertor from "./digitConvertor";
+// the following function gives one digit number a tenth place of zero . eg 0 -> 00 , 1->01 etc 
 
-const a = digitConvertor(4);
+function digitConvertor(number) {
+    switch (number) {
+        case 0:
+            return parseInt("00");
+            break;
+        case 1:
+            return parseInt("01");
+            break;
+        case 2:
+            return parseInt("02");
+            break;
+        case 3:
+            return parseInt("03");
+            break;
+        case 4:
+            return parseInt("04");
+            break;
+        case 5:
+            return parseInt("05");
+            break;
+        case 6:
+            return parseInt("06");
+            break;
+        case 7:
+            return parseInt("07");
+            break;
+        case 8:
+            return parseInt("08");
+            break;
+        case 9:
+            return parseInt("09");
+    }
+}
 
-console.log(a);
+
+
+function count 
