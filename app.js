14adf394555a9309b491d80a8ef0a43bf8df3f7b
mohit <mohitpardeshi729@gmail.com>
2022-12-29 20:12:18 +0530

    project completed , just adding social icons left

--- app.js
+++ app.js
@@ -34,6 +34,82 @@ function digitConvertor(number) {
     }
 }
 
+function monthIndexConvertor(input) {
+  return input-1;
+}
+
+function dateArgumentFormat() {
+    // this function converts different inputs into one javascript argument format
+    let targetDate = prompt("please type in the date");
+
+    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+    if(targetDate==""){
+        targetDate=10;
+    }
+    let targetMonth = prompt("type in the month");
+    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+    if(targetMonth==""){
+        targetMonth=5;
+    }
+    let targetYear = prompt("year");
+    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+    if(targetYear==""){
+        targetYear=2023;
+    }
+    let timeHour = prompt("hours");
+    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+    if(timeHour==""){
+        timeHour=12;
+    }
+    let timeMinutes = prompt("minutes");
+    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+    if(timeMinutes==""){
+        timeMinutes=59;
+    }
+    let timeSeconds = prompt("seconds");
+    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+    if(timeSeconds=""){
+        timeSeconds=59;
+    }
+
+    // javascript date method takes month input as indexes , meaning january = 0 and december = 11;
+     targetMonth = monthIndexConvertor(targetMonth);
+
+    result = targetMonth + " " + targetDate + ", " + targetYear + " , " + timeHour + ":" + timeMinutes + ":" + timeSeconds;
+    return result;
+
+}
 
 
-function count 
+
+
+let countDownDate = new Date(dateArgumentFormat()).getTime();
+
+function timeCounter() {
+
+
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Time out";
+    }
+
+
+}
+
+// Update the count down every 1 second
+setInterval(timeCounter, 1000);
