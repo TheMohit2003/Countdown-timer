44aa5b452ef5ab73e78361b7c16a3ff2fd53b7b1
mohit <mohitpardeshi729@gmail.com>
2022-12-30 12:00:30 +0530

    code reviewed , all working fie

--- app.js
+++ app.js
@@ -3,34 +3,37 @@
 function digitConvertor(number) {
     switch (number) {
         case 0:
-            return parseInt("00");
+            return "00";
             break;
         case 1:
-            return parseInt("01");
+            return "01";
             break;
         case 2:
-            return parseInt("02");
+            return "02";
             break;
         case 3:
-            return parseInt("03");
+            return "03";
             break;
         case 4:
-            return parseInt("04");
+            return "04";
             break;
         case 5:
-            return parseInt("05");
+            return "05";
             break;
         case 6:
-            return parseInt("06");
+            return "06";
             break;
         case 7:
-            return parseInt("07");
+            return "07";
             break;
         case 8:
-            return parseInt("08");
+            return "08";
             break;
         case 9:
-            return parseInt("09");
+            return "09";
+        default:
+            return number;
+            break;
     }
 }
 
@@ -38,52 +41,51 @@ function monthIndexConvertor(input) {
   return input-1;
 }
 
-function dateArgumentFormat() {
-    // this function converts different inputs into one javascript argument format
-    let targetDate = prompt("please type in the date");
+// function dateArgumentFormat() {
+//     // this function converts different inputs into one javascript argument format
+//     let targetDate = prompt('please type in the date .Incase you just want to see the countdown with default value , keep on clicking  "OK" on every message box');
 
-    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
-    if(targetDate==""|| targetDate===null){
-        targetDate=10;
-    }
-    let targetMonth = prompt("type in the month");
-    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
-    if(targetMonth==""|| targetMonth===null){
-        targetMonth=5;
-    }
-    let targetYear = prompt("year");
-    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
-    if(targetYear=="" || targetYear===null){
-        targetYear=2023;
-    }
-    let timeHour = prompt("hours");
-    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
-    if(timeHour=="" || timeHour===null){
-        timeHour=12;
-    }
-    let timeMinutes = prompt("minutes");
-    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
-    if(timeMinutes==""|| timeMinutes==null){
-        timeMinutes=59;
-    }
-    let timeSeconds = prompt("seconds");
-    // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
-    if(timeSeconds=""|| timeSeconds===null){
-        timeSeconds=59;
-    }
+//     // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+//     if(targetDate==""|| targetDate===null){
+//         targetDate=10;
+//     }
+//     let targetMonth = prompt("type in the month");
+//     // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+//     if(targetMonth==""|| targetMonth===null){
+//         targetMonth=5;
+//     }
+//     let targetYear = prompt("year");
+//     // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+//     if(targetYear=="" || targetYear===null){
+//         targetYear=2023;
+//     }
+//     let timeHour = prompt("hours");
+//     // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+//     if(timeHour=="" || timeHour===null){
+//         timeHour=12;
+//     }
+//     let timeMinutes = prompt("minutes");
+//     // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+//     if(timeMinutes==""|| timeMinutes==null){
+//         timeMinutes=59;
+//     }
+//     let timeSeconds = prompt("seconds");
+//     // the if statement is added to provide the program a default value in case the user doesnot gives any input value to the prompt
+//     if(timeSeconds=""|| timeSeconds===null){
+//         timeSeconds=59;
+//     }
 
-    // javascript date method takes month input as indexes , meaning january = 0 and december = 11;
-     targetMonth = monthIndexConvertor(targetMonth);
+//     // javascript date method takes month input as indexes , meaning january = 0 and december = 11;
+//      targetMonth = monthIndexConvertor(targetMonth);
 
-    result = targetMonth + " " + targetDate + ", " + targetYear + " , " + timeHour + ":" + timeMinutes + ":" + timeSeconds;
-    return result;
+//     result = targetMonth + " " + targetDate + ", " + targetYear + " , " + timeHour + ":" + timeMinutes + ":" + timeSeconds;
+//     return result;
 
-}
-
-
+// }
 
 
-let countDownDate = new Date(dateArgumentFormat()).getTime();
+
+let countDownDate = new Date("31 may , 2023 15:35:45").getTime();
 
 function timeCounter() {
 
@@ -98,17 +100,14 @@ function timeCounter() {
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 
-    document.getElementById('days').innerHTML = days;
-    document.getElementById('hours').innerHTML = hours;
-    document.getElementById('minutes').innerHTML = minutes;
-    document.getElementById('seconds').innerHTML = seconds;
+    document.getElementById('days').innerHTML = digitConvertor(days);
+    document.getElementById('hours').innerHTML = digitConvertor(hours);
+    document.getElementById('minutes').innerHTML = digitConvertor(minutes);
+    document.getElementById('seconds').innerHTML = digitConvertor(seconds);
 
     if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "Time out";
+        prompt("countdown stop");
     }
-
-    doc
 
 }
 
